11bc1dbd841b44fb15087fadc59eb017f332c641
jvillad <jyvillad@gmail.com>
2023-03-11 23:56:23 +0900

    adding stack navigation to home

--- mobile-app-inventory/App.js
+++ mobile-app-inventory/App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
 import Login from './src/screens/Login';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from './src/screens/Dashboard';
 
 // import Users from './Users';
 
 export default function App() {
+    const Stack = createStackNavigator();
+    const BottomTabs = createBottomTabNavigator();
     return (
-        <View style={styles.container}>
-            <Login />
-            {/* <StatusBar style="auto" /> */}
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                {/* <SafeAreaView style={styles.container}> */}
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Dashboard" component={Dashboard} />
+                {/* <Login />
+                    <Dashboard /> */}
+                {/* </SafeAreaView> */}
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
